5a253756aac1e21e350e2e992925ab2a16e65c5d
Adam Cameron <cameron.adam+github@gmail.com>
2022-04-20 20:44:22 +0100

    need to start ftp server as well

--- docker/restartContainers.sh
+++ docker/restartContainers.sh
@@ -11,3 +11,4 @@ clear; printf "\033[3J"
 docker-compose stop
 MARIADB_ROOT_PASSWORD=$1 MARIADB_PASSWORD=$2 docker-compose up --detach mariadb
 MARIADB_PASSWORD=$2 docker-compose up --detach lucee
+docker-compose up --detach ftp
